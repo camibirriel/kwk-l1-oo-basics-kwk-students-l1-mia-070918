d4277714ec06b8916ba7ab6b948cd9fafec0b8c1
camibirriel <camibirriel@gmail.com>
2018-07-12 17:40:36 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
-# book.rb
-# Add you Book class here
+class Book
+def initialize (title)
+  @title=title
+end
+ def title
+   return @title
+ end
+ def author=(author)
+   @author=author
+ end
+ def author 
+   @author
+ end
+ def page
+ @page 
+ end
+ 
+
+ end
